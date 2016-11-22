86d458c6c7b4cbf7d3d91766a15176448e504a85
some-subhra <somesubhrapatra@gmail.com>
2016-11-22 14:57:01 +0530

    updated and links

--- plunk/script.js
+++ plunk/script.js
@@ -1,20 +1,27 @@
 angular.module('infiniteScrollTutorial', ['infinite-scroll'])
-        .controller('infiniteScrollController', function ($scope, $http) {
+        .controller('infiniteScrollController', function ($scope, $http,$timeout) {
          
-	var urlPlus = 1993;
+	var urlPlus = 2004;
 	$scope.myData = [];
 	$scope.loading = false;
 	$scope.getMoreData = function () {
 		
 		$scope.movieYear = urlPlus;
-    		$http.get("http://www.omdbapi.com/?s=all&y="+urlPlus ).then(function(response) {
-			$scope.loading = true;
-			$scope.myData = $scope.myData.concat(response.data.Search);
+		$scope.loading = true;
+		$timeout(function(){
+		
+		$http.get("http://www.omdbapi.com/?s=all&y="+urlPlus ).then(function(response) {
+		
+		$scope.myData = $scope.myData.concat(response.data.Search);
+console.log($scope.myData)
 			
 			
     		}).finally(function(){
 			$scope.loading = false;
 		});
 		urlPlus++;
+		
+		},1500);
+    		
 	}
 });
